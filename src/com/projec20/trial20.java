88002600e212aafc9dc882b8c5d3d4706e18d206
rohitkapade <101393249+rohitkapade@users.noreply.github.com>
2022-07-31 17:42:18 +0530

    first

---
+++ src/com/projec20/trial20.java
@@ -0,0 +1,10 @@
+package com.projec20;
+
+public class trial20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("trial 20, thak gya hu bro");
+	}
+
+}
